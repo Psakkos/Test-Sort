e3756cddccecbfdd7bc84321737ed89927ab63da
Psakkos <31419207+Psakkos@users.noreply.github.com>
2017-10-06 14:38:19 -0500

    Add files via upload

--- TestSort.js
+++ TestSort.js
@@ -2,10 +2,11 @@ const Sort=require('./Sort.js');
 
 function main(){
   let biglist=[];
-    for(let x=0; x<100000; x++){
+    for(let x=0; x<1000000000; x++){
       biglist[x]=Math.random();
     }
-    let list1= Sort.insertion(biglist);
-    let list2= Sort.selection(biglist);
+    //let list1= Sort.insertion(biglist);
+    //let list2= Sort.selection(biglist);
+    let list3= Sort.merge(biglist);
 }
 main();
